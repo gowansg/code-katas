514540b58e2e25b1027cc9f8d9cd939e20178dfe
sgowan <sgowan@backgroundchecks.com>
2013-07-30 11:00:31 -0400

    initial commit -- splay tree is still a WIP

---
+++ splay-tree/csharp/CodeKatas/SplayTree.Tests/SplayTreeTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SplayTree.Tests
+{
+    [TestFixture]
+    public class SplayTreeTests
+    {
+        private IEnumerable<int> GenerateListOfConsecutiveInts(int count)
+        {
+            var list = new List<int>();
+            for (var i = 0; i < count; i++) list.Add(i);
+            return list;
+        }
+
+        [Test]
+        public void AllTheElementsInTheRangeUsedToBuildTheTreeAreAccountedFor()
+        {
+            var tree = new SplayTree<int>(GenerateListOfConsecutiveInts(256).ToArray());
+            for (int i = 0; i < 256; i++) Assert.IsNotNull(tree.Search(i));
+        }
+
+        [Test]
+        public void MostRecentlyAccessedNodeShouldBecomeTheRoot()
+        {
+            var tree = new SplayTree<int>(GenerateListOfConsecutiveInts(128).ToArray());
+            tree.Search(42);
+            //Assert.AreEqual(42, tree.Root);
+        }
+    }
+}
---
+++ splay-tree/csharp/CodeKatas/SplayTree/SplayTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplayTree
+{
+    public class SplayTree<T> where T : IComparable<T>
+    {
+        private Node<T> _root;
+
+        public SplayTree()
+        {
+        }
+
+        public SplayTree(T[] range)
+        {
+            _root = new Node<T>();
+            BuildTreeFromRange(_root, range);
+        }
+
+        private void BuildTreeFromRange(Node<T> newNode, T[] range)
+        {
+            //todo: sort the list first
+            if (range.Length == 0) return;
+
+            var middle = range.Length / 2;
+            newNode.Value = range[middle];
+
+            newNode.Right = new Node<T>();
+            var rightSubtree = range.Skip(middle)
+                                    .Take(range.Length - middle - 1)
+                                    .ToArray();
+            BuildTreeFromRange(newNode.Right, rightSubtree);
+
+            newNode.Left = new Node<T>();
+            var leftSubtree = range.Take(middle - 1).ToArray();
+            BuildTreeFromRange(newNode.Left, leftSubtree);
+        }
+
+        public Node<T> Search(T value)
+        {
+            return Search(_root, value);
+        }
+
+        private Node<T> Search(Node<T> currentNode, T value)
+        {
+            if (currentNode == null) return null;
+            if (value.CompareTo(currentNode.Value) == 0) return currentNode;
+            if (value.CompareTo(currentNode.Value) < 0) return Search(currentNode.Left, value);
+            if (value.CompareTo(currentNode.Value) > 0) return Search(currentNode.Right, value);
+
+            return null;
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+    }
+}
