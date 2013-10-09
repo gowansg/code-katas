52e4a7b1c8c7f8dc42547e8e9d2946813e254619
gowansg <gowansg@gmail.com>
2013-10-09 17:24:21 -0400

    Added check for a tie game

--- poker-hands/ruby/lib/poker/game.rb
+++ poker-hands/ruby/lib/poker/game.rb
@@ -10,7 +10,7 @@ module Poker
         declare_tie
       end
     end
-    
+
     private 
 
     def self.declare_winner(name, hand)
@@ -34,7 +34,15 @@ module Poker
       def winner
         sim = read(scenario)
         sim.keys.each { |k| sim[k] = Hand.new(@deck.deal(sim[k])) }
-        sim.sort_by { |player, hand| hand }.last
+        results = sim.sort_by { |player, hand| hand }
+        return nil if tied?(results)
+        results.last
+      end
+
+      def tied?(sim_results)
+        return false if sim_results.length < 2
+        highest_two_hands = sim_results.slice(-2, 2).collect { |x| x[1] }
+        (highest_two_hands[0] <=> highest_two_hands[1]) == 0
       end
 
       def num_of_players(amount = nil)
